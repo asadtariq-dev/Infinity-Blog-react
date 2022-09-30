42287015dc5cb82e63ac451878b9b2f4667ad874
asadtariq-dev <asad.tariq@devsinc.com>
2022-09-30 23:11:58 +0500

    Created project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import Posts from "./components/Posts";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import PostShow from "./components/PostShow";
+
+const API_URL = "http://localhost:3000/api/v1/posts";
+
+function getAPIData() {
+  return axios.get(API_URL).then((response) => response.data);
+}
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then((items) => {
+      if (mounted) {
+        setPosts(items);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-white mt-5 mb-5">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Posts posts={posts} />
+          </Route>
+          <Route exact path="/posts/:id">
+            <PostShow />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
